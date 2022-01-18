bcd24a64143e35d6894bcf62e19c691100ecd152
AdmiralEmser <admiralemser@gmail.com>
2022-01-18 16:26:13 +0100

    general structure

--- Chess_CS/Program.cs
+++ Chess_CS/Program.cs
@@ -6,10 +6,42 @@ using System.Threading.Tasks;
 
 namespace Chess_CS
 {
+    internal class GUI
+    {
+        public void start()
+        {
+
+        }
+        public void update()
+        {
+
+        }
+    }
+
+    internal class Game
+    {
+        // variables to store data
+        public short[,] field = new short[8, 8];
+        private short[,] possible_moves = new short[8, 8];
+
+        public void start()
+        {
+
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            // necessary classes and dependencies
+            Game game = new Game();
+            GUI graphics = new GUI();
+
+            // general structure of the game
+            game.start();
+            graphics.start();
+
         }
     }
 }
